65fea8dfac3f4cb22ae9cb91c4ab7d4b516da095
Mariusmichelnil <62707635+Mariusmichelnil@users.noreply.github.com>
2020-04-14 17:48:50 +0200

    Update chargin_station_test.py

--- chargin_station_test.py
+++ chargin_station_test.py
@@ -3,10 +3,13 @@ import os
 from numpy.random import randint
 from charging_station import ChargingStation
 
+charging_station = ChargingStation()
 
-CS = ChargingStation()
-t = 0
-load_battery = {"fast" : np.zeros((48,2)),"slow" : np.zeros((48,2))}
-load_battery = CS.update_batterie_stock(t,load_battery)
-print(load_battery)
+for t in range(48):
+    load = charging_station.compute_load(t)
+    charging_station.observe(t,0,0)
+    charging_station.penality(t)
+    print (load)
+print(charging_station.battery_stock)
 
+print("tests passed !")
