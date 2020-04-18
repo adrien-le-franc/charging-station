bbd4de028bf8373ae155d9e4ad85beddb5610304
Mariusmichelnil <62707635+Mariusmichelnil@users.noreply.github.com>
2020-04-18 10:04:18 +0200

    Update chargin_station_test.py

--- chargin_station_test.py
+++ chargin_station_test.py
@@ -8,8 +8,9 @@ charging_station = ChargingStation()
 for t in range(48):
     charging_station.observe(t,{"departures" : [15,15,15,15],"arrivals": [35,35,35,35]},{"internal":0,"external_sale":0,"external_purchase":0},0)
     load = charging_station.compute_load(t)
-    charging_station.penality(t)
+    charging_station.penalty(t)
     print (load)
 print(charging_station.bill)
+print(charging_station.battery_stock)
 
 print("tests passed !")
