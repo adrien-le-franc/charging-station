5b6d6387e5af471036c1245e6d36f1b072d3891f
Mariusmichelnil <62707635+Mariusmichelnil@users.noreply.github.com>
2020-04-23 20:36:44 +0200

    Update player_test.py

--- player_test.py
+++ player_test.py
@@ -6,13 +6,17 @@ from player import Player
 Player = Player()
 
 for t in range(48):
-    Player.observe(t,{"departures" : [15,15,15,15],"arrivals": [35,35,35,35]},{"internal":0,"external_sale":0,"external_purchase":0},0)
-    load = Player.compute_load(t)
+
+    Player.observe(t,{"departures" : [0,0,0,0],"arrivals": [0,0,0,0]},{"internal":0,"external_sale":0,"external_purchase":0},0)
+    load = Player.compute_load(t,0)
+    Player.nb_cars(0)
     Player.penalty(t)
     print ("load",load)
     print ("p_station",Player.p_station)
 print("load",Player.load_battery_periode)
 print(Player.bill)
 print(Player.battery_stock)
+print(Player.depart)
+
 
 print("tests passed !")
