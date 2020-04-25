eb0d0150a9e0024ad48c5606874d8878e00138d8
Mariusmichelnil <62707635+Mariusmichelnil@users.noreply.github.com>
2020-04-25 18:13:55 +0200

    Update player_test.py

--- player_test.py
+++ player_test.py
@@ -6,13 +6,18 @@ from player import Player
 Player = Player()
 
 for t in range(48):
-
-    Player.observe(t,{"departures" : [0,0,0,0],"arrivals": [0,0,0,0]},{"internal":0,"external_sale":0,"external_purchase":0},0)
+    if t==12:
+        Player.observe(t,{"departures" : [1,1,1,1],"arrivals": [0,0,0,0]},{"internal":0,"external_sale":0,"external_purchase":0},0)
+    elif t==36:
+        Player.observe(t,{"departures" : [0,0,0,0],"arrivals": [1,1,1,1]},{"internal":0,"external_sale":0,"external_purchase":0},0)
+    else:
+        Player.observe(t,{"departures" : [0,0,0,0],"arrivals": [0,0,0,0]},{"internal":0,"external_sale":0,"external_purchase":0},0)
     load = Player.compute_load(t,0)
-    Player.nb_cars(0)
     Player.penalty(t)
-    print ("load",load)
-    print ("p_station",Player.p_station)
+    print(load)
+
+print(Player.depart)
+print(Player.arrival)
 print("load",Player.load_battery_periode)
 print(Player.bill)
 print(Player.battery_stock)
